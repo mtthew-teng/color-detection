8d7df8dfd17822a640ee6a9c1913aff6eb61197c
mtthew-teng <196126034+mtthew-teng@users.noreply.github.com>
2025-02-04 17:24:51 -0800

    working blue detection

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import cv2
+from PIL import Image
+from util import get_limits
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+
+blue = [255, 0, 0]
 
 while True:
     ret, frame = cap.read()
 
-    if not ret:
-        print("Failed to capture frame")
-        break
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(color=blue)
+
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+    mask_ = Image.fromarray(mask)
+
+    bbox = mask_.getbbox()
+
+    print(bbox)
+
+    if bbox is not None:
+        x1, y1, x2, y2 = bbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
     cv2.imshow('frame', frame)
 
-    if cv2.waitKey(1000) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
